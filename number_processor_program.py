2835811492031ec15ce7f276b34115fa8719d461
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-26 21:29:23 +0800

    defined the program runflow as well as to execute it

--- number_processor_program.py
+++ number_processor_program.py
@@ -1,2 +1,15 @@
 from functionalities_of_program import NumberProcessor
 uploaded_file = "numbers.txt"
+
+
+def main():
+    processor = NumberProcessor()
+
+    if processor.read_numbers_from_file():
+        if processor.classify_numbers():
+            processor.write_even_numbers_to_file()
+            processor.write_odd_numbers_to_file()
+
+
+if __name__ == "__main__":
+    main()
