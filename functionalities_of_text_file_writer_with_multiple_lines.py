1c7c3a0e114ee39ea6a6a1e7235c7b0b954f161f
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-27 13:32:05 +0800

    wrote parameters to store the inputs of user into mylife txt file

--- functionalities_of_text_file_writer_with_multiple_lines.py
+++ functionalities_of_text_file_writer_with_multiple_lines.py
@@ -17,3 +17,20 @@ class TextFileWriter:
                 "Do you want to add more lines? (yes/no): ").strip().lower()
             if more in ['no', 'n', '']:
                 break
+
+    def write_to_file(self):
+        if not self.content:
+            print("No content to write. Please add some lines first.")
+            return False
+
+        try:
+            with open(self.filename, 'w') as file:
+                for line in self.content:
+                    file.write(line + '\n')
+
+            print(f"Content successfully written to '{self.filename}'")
+            return True
+
+        except Exception as error:
+            print(f"An error occurred while writing to the file: {error}")
+            return False
