48984281d59209e391987da60156c8f8aecd7b13
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-27 09:41:12 +0800

    created and defined parameters that will return student's name and its respective gwa

--- functionalities_of_highest_gwa_finder_program.py
+++ functionalities_of_highest_gwa_finder_program.py
@@ -2,3 +2,9 @@ class Student:
     def __init__(self, name, gwa):
         self.name = name.strip()
         self.gwa = float(gwa)
+
+    def __str__(self):
+        return f"{self.name} - GWA: {self.gwa:.2f}"
+
+    def __repr__(self):
+        return f"Student(name='{self.name}', gwa={self.gwa})"
