fe8a590f710c7b3c8bb40f6d6dc31e1a242de79b
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-27 14:38:23 +0800

    created a class named integer processor and initialized the class and created empty lists of integers, even, odd, even squared, and odd cubed

---
+++ functionalities_of_the_integer_processor_squaring_even_and_cubing_odd.py
@@ -0,0 +1,8 @@
+class IntegerProcessor:
+    def __init__(self, source_file="integers.txt"):
+        self.source_file = source_file
+        self.integers = []
+        self.even_integrs = []
+        self.odd_integers = []
+        self.even_squared = []
+        self.odd_cubed = []
