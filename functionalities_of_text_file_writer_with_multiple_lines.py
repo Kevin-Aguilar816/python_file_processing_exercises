1f35a8b350a350cf19c98d14ee13cb0f901adfdc
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-27 13:21:05 +0800

    created a parameter to get the user input and to prompt the user to type done to notify the program that the user is already done

--- functionalities_of_text_file_writer_with_multiple_lines.py
+++ functionalities_of_text_file_writer_with_multiple_lines.py
@@ -3,4 +3,12 @@ class TextFileWriter:
         slef.filename = filename
         self.content = []
 
-        pass
+    def get_user_input(self):
+        while True:
+            line = input("Enter a line of text (or 'done' to finish): ")
+            if line.lower() == 'done':
+                break
+            self.content.append(line)
+            print(f"Added: '{line}'")
+            else:
+                print("Invalid input. Please enter a line of text or 'done' to finish.")
