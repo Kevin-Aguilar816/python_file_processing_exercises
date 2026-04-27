1e3565e6a93edf7b58f1f4aef2290667ad899837
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-27 09:52:42 +0800

    created a class named student record processor and initialized it to process and determine the student with the highest gwa and its name

--- functionalities_of_highest_gwa_finder_program.py
+++ functionalities_of_highest_gwa_finder_program.py
@@ -8,3 +8,10 @@ class Student:
 
     def __repr__(self):
         return f"Student(name='{self.name}', gwa={self.gwa})"
+
+
+class StudentRecordProcessor:
+    def __init__(self, input_filename="students.txt"):
+        self.input_filename = input_filename
+        self.students = []
+        self.top_student = None
